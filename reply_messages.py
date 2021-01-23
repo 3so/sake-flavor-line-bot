42b62358487199aa676a468547bb2b90bce19f20
3so <11akihiro10@gmail.com>
2021-01-23 18:28:51 +0900

    検索ワードの改行の削除

--- reply_messages.py
+++ reply_messages.py
@@ -1,6 +1,8 @@
 import get_sakenowa_api as gs
 
 def brand_list(event):
+    search_word = event.message.text
+    search_word = search_word.replace("\n","")
     search_brands = gs.search_brands(event.message.text)
     if search_brands == []:
         return "該当する銘柄が見つかりませんでした"
