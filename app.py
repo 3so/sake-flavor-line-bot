401ac76840e5635daa643e98957f7d1dcc949361
3so <11akihiro10@gmail.com>
2021-02-06 14:26:17 +0900

    銘柄一覧を返却するように修正

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # brand_list = rm.brand_list(event)
+    brand_list = rm.brand_list(event)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=brand_list)
