d7d56803306a01e76376514c11f8f10c2262fe6b
3so <69714545+3so@users.noreply.github.com>
2021-02-21 16:33:58 +0900

    Heroku上の日本語フォントの設定

--- flavor_chart.py
+++ flavor_chart.py
@@ -12,14 +12,14 @@ def get_flavor_chart(flavor):
     df = df.rename(columns={'f1':'華やか', 'f2':'芳醇', 'f3':'重厚', 'f4':'穏やか', 'f5':'ドライ', 'f6':'軽快'}).T
 
     fig = px.line_polar(df, r=df[0], theta=df.index, line_close=True, range_r=[0,1], title=(flavor["brandName"] + "のフレーバーチャート"))
-    fig.update_layout(width=600, font=dict(size=22))
+    fig.update_layout(width=600, font_family="IPAexゴシック", font=dict(size=22))
     fig.write_image('static/flavor_chart.png')
 
 
 # テスト
 flavor = {
     "brandId": 2,
-    "f1": 0.5,
+    "f1": 0.7,
     "f2": 0.424835098066976,
     "f3": 0.353698484182939,
     "f4": 0.480473334729331,
